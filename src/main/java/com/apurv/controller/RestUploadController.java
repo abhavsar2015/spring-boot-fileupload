5790a482f2ea80addf649ea3fd6d826ed428802a
abhavsar2015 <bapurv557@gmail.com>
2017-06-19 11:24:20 -0400

    third commit

--- src/main/java/com/apurv/controller/RestUploadController.java
+++ src/main/java/com/apurv/controller/RestUploadController.java
@@ -2,23 +2,34 @@ package com.apurv.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.apurv.entity.Login;
 import com.apurv.model.Metadata;
+import com.apurv.service.FileMetadatServices;
+import com.apurv.services.EmployeeService;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,7 +41,9 @@ public class RestUploadController {
      //Save the uploaded file to this folder
     private static String UPLOADED_FOLDER = "D://demo//";
 
-   
+    @Autowired
+	FileMetadatServices fileSer;
+
 
     // Multiple file upload
     @PostMapping("/api/upload/multi")
@@ -56,13 +69,27 @@ public class RestUploadController {
         return new ResponseEntity("Successfully uploaded - "
                 + uploadedFileName, HttpStatus.OK);
     }
+    @RequestMapping(value = "/Metadata", method = RequestMethod.POST,produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	 @ResponseBody
+	 public ResponseEntity<List<Metadata>> getMatadata(@RequestBody Metadata metadata1 ) {
+	       
+	        List<Metadata> metadata= fileSer.getAllMetadata();
+	        System.out.println(metadata);
+		    return new ResponseEntity<List<Metadata>>(metadata, HttpStatus.OK);
+	    }
     private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
     for (MultipartFile file : files) {
 
             if (file.isEmpty()) {
                 continue; 
             }
-
+            Date dNow = new Date( );
+            SimpleDateFormat ft = 
+            new SimpleDateFormat ("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+            Metadata m=new Metadata();
+            m.setFiles(file.getOriginalFilename());
+            m.setCurrentTime(ft.format(dNow));
+            fileSer.addMetadata(m);
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
--- src/main/java/com/apurv/repository/FileMetadataRepository.java
+++ src/main/java/com/apurv/repository/FileMetadataRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 import com.apurv.model.Metadata;
 
@@ -15,15 +17,27 @@ public class FileMetadataRepository implements FileMetadataRepositoryDef{
     public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
             this.transactionTemplate = transactionTemplate;
         }
+    public EntityManager getEm() {
+		return em;
+	}
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
 
+    @Override
+	@Transactional
 	public String addMetadata(Metadata m)
 	{
+		EntityManager em1 = getEm();
+		//em1.getTransaction().begin();
+   	   em1.persist(m);
+	   return m.getFileId();
 		
-		return "";
 	}
+    @Override
     public List<Metadata> getAllMetadata()
     {
-    	TypedQuery<Metadata> tq=em.createNamedQuery("Movie.findAll", Metadata.class);
+    	TypedQuery<Metadata> tq=em.createNamedQuery("Metadata.findAll", Metadata.class);
 		return tq.getResultList();
     }
 }
